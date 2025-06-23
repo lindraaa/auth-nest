8571211af9398cc03285a461931aa218c14de2ee
lindraaa <chancruz739@gmail.com>
2025-06-23 14:07:12 +0800

    setup for database, swagger for api documentation and the validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Auth API')
+    .setDescription('')
+    .setVersion('1.0')
+    // .addBearerAuth({
+    //   type: 'http',
+    //   scheme: 'bearer',
+    //   bearerFormat: 'JWT',
+    //   in: 'header',
+    //   name: 'Authorization',
+    //   description: 'Enter your token',
+    // })
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger/api', app, documentFactory);
+
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
